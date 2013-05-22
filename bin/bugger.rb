ff5ef35fe4b8cc0ee562b200e4cd10e7c5abbf6b
Anders Brujordet <anb@knowit.no>
2013-05-22 22:36:17 +0200

    Logical failures after upgrade of db

--- bin/bugger.rb
+++ bin/bugger.rb
@@ -12,7 +12,7 @@ class Bugger
     end
 
     def get_last_task()
-        sql="select time_id from time where timestop is null"
+        sql="select task_id from time where timestop is null"
         result = @db.execute(sql)
         if (result.empty?)
             nil
@@ -27,10 +27,11 @@ class Bugger
     end
 
     def time_spent_by(id)
+        puts "Checking time spent for task_id: #{id}"
         sql = "select strftime('%s',timeStart), strftime('%s','now') from time where task_id=?"
         row = @db.execute(sql, id)[0]
-        timestart = row.to_i
-        now = row.to_i        
+        timestart = row[0].to_i
+        now = row[1].to_i        
         seconds = now - timestart
         minutes = seconds / 60
         hours = minutes / 60
@@ -43,7 +44,6 @@ class Bugger
         task_id = get_last_task
         if (task_id == nil)
             prompt
-            exit
         else
             title = "Time spent on task: " + time_spent_by(task_id)
             task_name = get_task_name(task_id)
@@ -60,18 +60,27 @@ class Bugger
     end
 
     def end_current(id)
-        if (!id == nil)
+        if (id != nil)
             sql = "update time set timeStop = DATETIME('now') where task_id=?"
             @db.execute(sql, id)
         end
     end
 
     def register_new_task(task_name)
-        puts "registering new task: #{task_name}"
-        sql_task = "insert into task values(null, ?, null)"
-        @db.execute(sql_task, task_name)
         sql_task_id = "select task_id from task where name=?"
-        task_id = @db.execute(sql_task,task_name)[0]
+        task_id_row = @db.execute(sql_task_id,task_name)
+        if (task_id_row.empty?)            
+            puts "registering new task: #{task_name}"
+            sql_task = "insert into task values(null, ?, null)"
+            @db.execute(sql_task, task_name)
+
+            sql_task_id = "select task_id from task where name=?"
+            task_id = @db.execute(sql_task_id,task_name)[0]
+        else
+            puts "Found existing id for task: #{task_name}"
+            task_id=task_id_row[0]
+        end
+        
         sql_time = "insert into time values(null, DATETIME('now'), null, null, ?)"
         @db.execute(sql_time, task_id)
     end
@@ -87,7 +96,6 @@ class Bugger
         end
         new_task = %x(#{@cocoa} standard-inputbox --title "Bugger - What are you working on?" --text "#{task_name}" --float --no-newline --no-cancel --informative-text "Time spent on current task: #{time_spent}" | tail -n 1 )
         if (task_name != new_task)
-            #TODO check for null
             end_current(task_id)
             register_new_task(new_task)
         end
